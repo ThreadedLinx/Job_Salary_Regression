242ac3a7f94433e6cd3d633cb465adc66a62a7cd
Nay <caeserome@gmail.com>
2022-07-19 12:07:33 -0700

    Storing Job Title, Company Name, Location to Variables - Un Refractored

--- Indeed_scrape_Test.py
+++ Indeed_scrape_Test.py
@@ -106,6 +106,18 @@ try:
 
     for salary in job_list_with_salary_listed:
 
+        #Job Title
+        job_title = salary.find_element(By.CLASS_NAME, "jobTitle")
+        # //*[@span="title"]
+        #"*//div[@class='attribute_snippet']"
+        print(job_title.text)
+
+        company_name = salary.find_element(By.CLASS_NAME, "companyName")
+        print(company_name.text)
+
+        location = salary.find_element(By.CLASS_NAME, "companyLocation")
+        print(location.text)
+
         salary_test_three = salary_string = WebDriverWait(salary, 5).until(
         EC.presence_of_element_located((By.XPATH, "*//div[@class='metadata salary-snippet-container' or @class='metadata estimated-salary-container']"))
         )
@@ -119,48 +131,195 @@ try:
                 salary_list.append(word)
             print(salary_list)
 
+            #Index of $ +1 and K Because Tagret Numbers Lie In between these Characters - First Number In Sequence
             sal_min_start = salary_list.index('$') + 1
             sal_min_end = salary_list.index('K')
 
             print(sal_min_start)
             print(sal_min_end)
 
+            #Slicing Target Number Using Specific Index of $ + 1 and K
             sal_min_list = salary_list[sal_min_start:sal_min_end]
 
             print(sal_min_list)
 
-            string_min_list = ''.join(sal_min_list)
+            #Joining Min Numbers Into a String
+            string_min_string = ''.join(sal_min_list)
 
-            print(string_min_list)
-            print(type(string_min_list))
+            print(string_min_string)
+            print(type(string_min_string))
 
-            float_min = float(string_min_list)
+            #Turning String of Min Into a Float
+            float_min = float(string_min_string)
             print(float_min)
             print(type(float_min))
 
             print('Getting Max')
 
+            #Index of $ +1 and K Because Tagret Numbers Lie In between these Characters - Second Number In Sequence
             sal_max_start = salary_list.index('$', 17) + 1
             sal_max_end = salary_list.index('K', 17)
 
             print(sal_max_start)
             print(sal_max_end)
 
+            #Slicing Target Number Using Specific Index of $ + 1 and K
             sal_max_list = salary_list[sal_max_start:sal_max_end]
             print(sal_max_list)
 
-            string_max_list = ''.join(sal_max_list)
+            #Joining Max Numbers Into A String
+            string_max_string = ''.join(sal_max_list)
             
-            print(string_max_list)
-            print(type(string_max_list))
+            print(string_max_string)
+            print(type(string_max_string))
 
-            float_max = float(string_max_list)
+            #Turning String Of Max Into A Float
+            float_max = float(string_max_string)
             print(float_max)
             print(type(float_max))
 
-            float_median = (float_max + float_min) / 2
 
-            print(float_median)
+            #Getting The Mean Of The Salary Range
+            float_mean = (float_max + float_min) / 2
+
+            print(float_mean)
+
+        elif "Estimated" not in salary_test_three.text and "a year" in salary_test_three.text:
+
+            print('This Is A Year String Only')
+            print(salary_test_three.text)
+
+            for word in salary_test_three.text:
+                salary_list.append(word)
+            print(salary_list)
+
+            #Index of $ +1 and *Space* Because Tagret Numbers Lie In between these Characters - First Number In Sequence
+            year_salary_min_start = salary_list.index('$') + 1
+            year_salary_min_end = salary_list.index(' ')
+
+            print(year_salary_min_start)
+            print(year_salary_min_end)
+
+            #Slicing Target Number Using Specific Index of $ + 1 and *Space*
+            year_sal_min_list = salary_list[year_salary_min_start:year_salary_min_end]
+            print(year_sal_min_list)
+
+            #Removing Comma From List
+            year_sal_min_list.remove(',')
+            print(year_sal_min_list)
+
+            #Joining Min Numbers Into A String
+            string_year_min = ''.join(year_sal_min_list)
+            
+            print(string_year_min)
+            print(type(string_year_min))
+
+            #Turning Minimum Salary String Into A Float
+            float_yearly_min = float(string_year_min)
+            print(float_yearly_min)
+            print(type(float_yearly_min))
+
+
+            print('Now Getting Max Value ...')
+            #Index of $ +1 and *Space* Starting At Specific Index Because Tagret Numbers Lie In between these Characters - Second Number In Sequence
+            yearly_max_start = salary_list.index('$', 9) + 1
+            yearly_max_end = salary_list.index('a', 9) - 1
+
+            print(yearly_max_start)
+            print(yearly_max_end)
+
+            #Slicing Target Number Using Specific Index of $ + 1 and 'a' - 1
+            year_sal_max_list = salary_list[yearly_max_start:yearly_max_end]
+            print(year_sal_max_list)
+
+            #Removing Comma
+            year_sal_max_list.remove(',')
+
+            #Joining Max Numbers Into A String
+            string_year_max = ''.join(year_sal_max_list)
+
+            print(string_year_max)
+            print(type(string_year_max))
+
+            #Turning Maximum Salary String Into A Float
+            float_yearly_max = float(string_year_max)
+            print(float_yearly_max)
+            print(type(float_yearly_max))
+
+            #Getting Mean of Salary Range
+            float_yearly_mean = (float_yearly_min + float_yearly_max) / 2
+
+            print(float_yearly_mean)
+
+        else:
+            print(salary_test_three.text)
+            print('Should Be By The Hour')
+
+            for word in salary_test_three.text:
+                salary_list.append(word)
+            print(salary_list)
+
+            #Index of $ + 1 and '-' minus 1
+            hourly_min_start = salary_list.index('$') + 1
+            hourly_min_end = salary_list.index('-') - 1
+            print(hourly_min_start)
+            print(hourly_min_end)
+
+            #Slicing Target Number Using Specifc Index of $ + 1 and '-' Minus 1
+            hourly_salary_min_list = salary_list[hourly_min_start:hourly_min_end]
+            print(hourly_salary_min_list)
+
+            #Joining Numbers Into A String
+            hourly_sal_min_string = ''.join(hourly_salary_min_list)
+
+            print(hourly_sal_min_string)
+            print(type(hourly_sal_min_string))
+
+            #Turning Min Salary String Into A Float
+            float_hourly_min = float(hourly_sal_min_string)
+
+            print(float_hourly_min)
+            print(type(float_hourly_min))
+
+            #Getting Max Hourly Value
+            print('Now Getting Hourly Max Value')
+
+            #Index Of $ + 1 and 'a' minus 1 Starting At Specific Index Because Tagret Numbers Lie In between these Characters - Second Number In Sequence
+            hourly_max_start = salary_list.index('$', 4) + 1
+            hourly_max_end = salary_list.index('a', 4) - 1
+            
+            print(hourly_max_start)
+            print(hourly_max_end)
+
+            #Slicing Target Number Using Specifc Index Of $ + 1 and 'a' minus 1
+            hourly_sal_max_list = salary_list[hourly_max_start:hourly_max_end]
+            print(hourly_sal_max_list)
+
+            #Joing Max Number Into A String
+            hourly_sal_max_string = ''.join(hourly_sal_max_list)
+
+            print(hourly_sal_max_string)
+            print(type(hourly_sal_max_string))
+
+            #Turning Max Hourly Salary String Into A Float
+            float_hourly_max = float(hourly_sal_max_string)
+            print(float_hourly_max)
+            print(type(float_hourly_max))
+
+            #Getting Mean of Hourly Salary Range
+            float_hourly_mean = (float_hourly_min + float_hourly_max) / 2
+            print(float_hourly_mean)
+
+            #Converting Hourly Mean To Yearly Salary
+            float_hourly_mean_converted_to_yearly = float_hourly_mean * 40 * 52
+
+            print(float_hourly_mean_converted_to_yearly)
+
+
+
+
+
+
 
 
 
