0015e5eb3d6c068ccc5d8f6185213cf46aaf3483
Nay <caeserome@gmail.com>
2022-07-20 12:28:32 -0700

    Gets Job Info For Up To 5 Pages And Displays It In Data Frame.  City and Job Are Static

--- Indeed_scrape_Test.py
+++ Indeed_scrape_Test.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
+
 import time
 import pandas as pd
 
@@ -19,98 +20,25 @@ PATH = "/Users/nay/Desktop/Development/Drivers/chromedriver"
 driver = webdriver.Chrome(PATH)
 driver.maximize_window()
 
-
 #Going To Website
 driver.get("https://www.indeed.com/jobs?q=ios%20developer&l=Los%20Angeles")
 
-# jobsearch-ResultsList css-0
 
+#Variables
 
-#Finding Job List
-try:
-    job_list = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="mosaic-provider-jobcards"]/ul'))
-    )
-
-    #print('Job List Found')
-
-except:
-    print('Couldnt Find List Of Jobs')
-
-
-#Creating A List of 'Li' Elements Which Is Each Job Listing On This Job Board Website
-try:
-    jobs = job_list.find_elements_by_tag_name('li')
-    #print(jobs)
-except:
-    print('No salary Found')
-
+#List Of Pages As Web Elements
+pages = []
+#List of All Postings that have the salary listed - Emptied Every Loop
+job_list_with_salary_listed = []
 #Post That Contain these strings have the salary listed
 money_matches = ['an hour', '$', 'a year', 'Estimated']
-
-#List of All Postings that have the salary listed
-job_list_with_salary_listed = []
-
-#For Loop to append each listing that contains salary information to the job_with_salary_listed list
-for job in jobs:
-
-    listing = job.text
-
-    #Checking if any strings in the listing contain strings in money matches - signifying the salary is present
-    if any(x in listing for x in money_matches):
-        #Appending Job Listing That Matches Criteria To job_with_salary_listed list
-        job_list_with_salary_listed.append(job)
-        #Checking if Length of List is Growing
-        print(len(job_list_with_salary_listed))
-
-
-#These are the class name identifiers for each type of salary listed
-#x - x an hour --- metadata salary-snippet-container
-# Estimated Salary x - x --- metadata estimated-salary-container
-#$xx - $xx -- metadata salary-snippet-container
-#Stored each in a lisg
-html_list = ['metadata salary-snippet-container', 'metadata estimated-salary-container', 'metadata salary-snippet-container']
-
-try:
-
-    #print(job_list_with_salary_listed)
-
-
-    #Checking if Jobs in jobs_list all have a class tag to access the salary data
-    for job in job_list_with_salary_listed:
-
-        #print('Inner Html For Job ... ')
-        get_html = job.get_attribute('innerHTML')
-        #print(get_html)
-
-        #If the inner HTML contains any string from the html_list - The Salary Data will be accessible
-        if any(x in get_html for x in html_list):
-            print('Salary Html Is Present')
-        else:
-            print('Missing Class')
-
-
-except:
-    print('Something Went Wrong')
-
-
-#Do a xpath with class and contains 'Salary' as text
-#*//div[contains(@class,'salary')]
-
-
-#salary_section = []
-
 #List of Job Objects That Contains Position, Company, Location, and Salary
 job_postings = []
 
-#Try Block - Getting all relevant data and storing it in a job dictionary then appending it to the job postings list
-try:
-    #print('Getting By Xpath Contains Text .... ')
+#Functions
 
-    #This Was A Check To See How To Access The Salary Information
-    # salary_test_one = WebDriverWait(job_list_with_salary_listed[2], 5).until(
-    #     EC.presence_of_element_located((By.XPATH, "*//div[contains(@class,'salary')]"))
-    # )
+#This Function Grabs The Necessary Data From Each Job Listing: Position, Company Name, Location, and Salary
+def get_job_info():
 
     for salary in job_list_with_salary_listed:
 
@@ -347,12 +275,184 @@ try:
 
         job_postings.append(job_object)
 
+#This Function Populates job_list_with_salary_listed
+def get_jobs_with_salary_listed():
 
-except:
-    print('Xpath Text Contains Failed')
+    #Finding Job List
+    try:
+        job_list = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="mosaic-provider-jobcards"]/ul'))
+        )
+        #print('Job List Found')
+
+        #Once Job List Is Found - Create A List Of 'Li' Elements Which Is Each Job Listing On This Job Board Website
+        try:
+            jobs = job_list.find_elements_by_tag_name('li')
+            #print(jobs)
+
+            try: 
+                #For Loop to append each listing that contains salary information to the job_with_salary_listed list
+                for job in jobs:
+
+                    listing = job.text
+
+                    #Checking if any strings in the listing contain strings in money matches - signifying the salary is present
+                    if any(x in listing for x in money_matches):
+                        #Appending Job Listing That Matches Criteria To job_with_salary_listed list
+                        job_list_with_salary_listed.append(job)
+                        #Checking if Length of List is Growing
+                        print(len(job_list_with_salary_listed))
+            except:
+                print('Failed To Loop Through List and Extract Jobs That Contain Salaries')
+        except:
+            print('Failed To Extract Each Job Element')
+    except:
+        print('Failed To Find List Of Jobs')
+
+#This Function isnt 'Necessary' - Its a Checkpoint To Make Sure Salary Info Is Listed Before Extraction Of Data Is Performed
+def salary_checkpoint():
+    #These are the class name identifiers for each type of salary listed
+    #x - x an hour --- metadata salary-snippet-container
+    # Estimated Salary x - x --- metadata estimated-salary-container
+    #$xx - $xx -- metadata salary-snippet-container
+    #Stored each in a lisg
+    html_list = ['metadata salary-snippet-container', 'metadata estimated-salary-container', 'metadata salary-snippet-container']
+
+    try:
+
+        #print(job_list_with_salary_listed)
+        #Checking if Jobs in jobs_list all have a class tag to access the salary data
+        for job in job_list_with_salary_listed:
+
+            #print('Inner Html For Job ... ')
+            get_html = job.get_attribute('innerHTML')
+            #print(get_html)
+
+            #If the inner HTML contains any string from the html_list - The Salary Data will be accessible
+            if any(x in get_html for x in html_list):
+                #print('Salary Html Is Present')
+                pass
+            else:
+                print('Missing Salary Class')
+        
+        #Communicate To Console Final Check Is Successful
+        print('Data is Present: Continue To Extraction')
+    except:
+        print('Something Went Wrong: Salary Items Missing')
+
+#This Function Holds All Above Functions And Executes Them In Order
+def get_data():
+    get_jobs_with_salary_listed()
+    salary_checkpoint()
+    try:
+        get_job_info()
+    except:
+        print('Data Scrape Failed: Could not Retreive Info')
+
+#This Function Clicks Away From Any PopUp
+def avoid_pop_up():
+    #Closing Popups If They Exist
+    try:
+        #Locating 'x' symbol on pop window
+        popup_window = WebDriverWait(driver, 7).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@class='popover-x-button-close icl-CloseButton']"))
+        )
+        print('Pop Up Appeared')
+
+        print('Closing Pop Up With Action Chain')
+
+        #Moving Mouse
+        #Create action chain object
+        action = ActionChains(driver)
+        #Moving Mouse Away From Pop Up
+        action.move_to_element(popup_window)
+        action.click(popup_window)
+        action.move_to_element_with_offset(popup_window, 40, 50)
+        action.click()
+        action.perform()
+
+        print('Pop Up Window Closed')
+
+    except:
+        print('No Pop Up, Continue Scraping')
+
+
+#Get Page Link (i.e. "1", "2", "3", "4") Elements In Global Scope for pages variable
+def get_pages():
+    try:
+        page_count = WebDriverWait(driver, 5).until(
+         EC.presence_of_element_located((By.CLASS_NAME, "pagination-list"))
+        )
+        print('Got page count')
+
+        #Get Each Element and Populate Pages List
+        try:
+            pages_buttons = page_count.find_elements_by_tag_name("li")
+            #pages = pages_buttons
+            #print('Success???')
+            print(pages)
+
+            return pages_buttons
+
+        except:
+            print('Page Link Elements Failed To Be Found or Populated')
+
+    except:
+        print('Page UL Failed To Be Found')
+
+#Locate Next Arrow - If Exist Click It and Go To Next Page
+def next_page():
+    try:
+        #clickable arrow
+        arrow = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "np"))
+        )
+
+        arrow.click()
+        print('Going To Next Page')
+
+    except:
+        print('This Was The Final Page')
+
+#Setting Pages Variable To Returned list Of Page Link Web Elements
+pages = get_pages()
+
+for page in pages:
+
+    #List of All Postings that have the salary listed - Emptied Every Loop
+    job_list_with_salary_listed = []
+
+    time.sleep(2)
+    get_data()
+    time.sleep(2)
+    next_page()
+    avoid_pop_up()
 
 
 #Putting Job Postings List Into Data Frame
 df = pd.DataFrame(job_postings)
 print(df)
+
+
+
+
+#Do a xpath with class and contains 'Salary' as text
+#*//div[contains(@class,'salary')]
+
+#Try Block - Getting all relevant data and storing it in a job dictionary then appending it to the job postings list
+# try:
+#     #print('Getting By Xpath Contains Text .... ')
+
+#     #This Was A Check To See How To Access The Salary Information
+#     # salary_test_one = WebDriverWait(job_list_with_salary_listed[2], 5).until(
+#     #     EC.presence_of_element_located((By.XPATH, "*//div[contains(@class,'salary')]"))
+#     # )
+
+#     get_data()
+# except:
+#     print('Xpath Text Contains Failed')
+
+
+
+
 
